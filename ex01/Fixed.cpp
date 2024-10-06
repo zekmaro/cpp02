f42c9aef31d6b19fbbff7917b37d4ac2c7ddde2e
Andrey Arama <anarama@c2r7p4.42vienna.com>
2024-10-06 17:08:02 +0200

    ex01

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,11 +6,12 @@
 /*   By: anarama <anarama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 12:42:18 by anarama           #+#    #+#             */
-/*   Updated: 2024/10/06 17:03:29 by anarama          ###   ########.fr       */
+/*   Updated: 2024/10/06 17:04:03 by anarama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cmath>
 
 #include "Fixed.hpp"
 
@@ -22,7 +23,25 @@ Fixed::Fixed( void ) : _integer(0) {
 
 Fixed::Fixed(const Fixed& other) {
     std::cout << "Copy constructor called" << std::endl;
-    _integer = other._integer;
+    _integer = other._integer;  // Copy the raw value
+}
+
+Fixed::Fixed( const int integer ) {
+	std::cout << "Int constructor called" << std::endl;
+	this->_integer = integer << _num_fraction_bits;
+}
+
+Fixed::Fixed( const float float_number ) {
+	std::cout << "Float constructor called" << std::endl;
+	this->_integer = roundf(float_number * (1 << _num_fraction_bits));
+}
+
+float	Fixed::toFloat( void ) const {
+	return (float)this->_integer / (1 << _num_fraction_bits);
+}
+
+int		Fixed::toInt( void ) const {
+	return this->_integer >> _num_fraction_bits;
 }
 
 Fixed& Fixed::operator=(const Fixed& other) {
@@ -31,6 +50,11 @@ Fixed& Fixed::operator=(const Fixed& other) {
 		this->_integer = other._integer;
 	}
 	return *this;
+}
+
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed) {
+    out << fixed.toFloat();
+    return out;
 }
 
 Fixed::~Fixed( void ) {
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -6,12 +6,14 @@
 /*   By: anarama <anarama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/05 12:42:22 by anarama           #+#    #+#             */
-/*   Updated: 2024/10/06 17:03:01 by anarama          ###   ########.fr       */
+/*   Updated: 2024/10/06 16:37:22 by anarama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
+
+# include <iostream>
 
 # define DEFAULT_INTEGER 0
 
@@ -23,11 +25,17 @@ class Fixed {
 	public:
 		Fixed( void );
 		Fixed( const Fixed& other );
+		Fixed( const int integer );
+		Fixed( const float float_number );
 		Fixed& operator=(const Fixed& other);
 		~Fixed( void );
 
 		int		getRawBits( void ) const;
 		void	setRawBits( int const raw );
+		float	toFloat( void ) const;
+		int		toInt( void ) const;
 };
+
+std::ostream& operator<<(std::ostream& out, const Fixed& other);
 
 #endif // FIXED_HPP
